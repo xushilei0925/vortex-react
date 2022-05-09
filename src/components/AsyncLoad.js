73e2d5c366aa5af8f32d07d1aa50aae67b0447ee
xushilei0925 <75980000+xushilei0925@users.noreply.github.com>
2022-05-09 16:27:46 +0800

    22
    22

--- src/components/AsyncLoad.js
+++ src/components/AsyncLoad.js
@@ -2,7 +2,7 @@ import Loadable from 'react-loadable'
 import Loading from './Loading'
 
 export default function AsyncLoad(opts) {
-  return Loadable({
+  retur
     ...opts,
     loading: Loading,
     delay: 200,
